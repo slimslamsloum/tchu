0e95586834c81e5f6b5392a35f8835267943971f
Selim Jerad <selim.jerad@epfl.ch>
2021-03-21 17:06:53 +0100

    etape 5

---
+++ src/ch/epfl/tchu/game/PublicGameState.java
@@ -0,0 +1,73 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+
+import java.util.List;
+import java.util.Map;
+
+public class PublicGameState {
+    private final int ticketCount;
+    private final PublicCardState cardState;
+    private final PlayerId currentPlayerId;
+    private final Map<PlayerId, PublicPlayerState> playerState;
+    private final PlayerId lastPlayer;
+
+
+    public PublicGameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId, Map<PlayerId, PublicPlayerState> playerState, PlayerId lastPlayer){
+        Preconditions.checkArgument(ticketsCount >= 0 && playerState.size() == 2);
+        if (cardState == null || currentPlayerId == null){
+            throw new NullPointerException("current Player Id or cardstate is null");
+        }
+        this.ticketCount =ticketsCount;
+        this.cardState=cardState;
+        this.currentPlayerId=currentPlayerId;
+        this.playerState=playerState;
+        this.lastPlayer=lastPlayer;
+
+    }
+
+    public int ticketsCount(){
+        return ticketCount;
+    }
+
+    public boolean canDrawTickets(){
+        if (cardState.isDeckEmpty()){
+            return false;
+        }
+        else return true;
+    }
+
+    public PublicCardState cardState(){
+        return cardState;
+    }
+
+    public boolean canDrawCards(){
+        if (cardState.totalSize() >= 10){
+            return true;
+        }
+        else return false;
+    }
+
+    public PlayerId currentPlayerId(){
+        return currentPlayerId;
+    }
+
+    public PublicPlayerState playerState(PlayerId playerId){
+        return playerState.get(playerId);
+    }
+
+    public PublicPlayerState currentPlayerState(){
+        return playerState.get(currentPlayerId);
+    }
+
+    public List<Route> claimedRoutes(){
+        return playerState.get(currentPlayerId).routes();
+    }
+
+    public PlayerId lastPlayer(){
+        if (lastPlayer == null){
+            return null;
+        }
+        else return lastPlayer;
+    }
+}
--- test/ch/epfl/tchu/game/arbitraryTest.java
+++ test/ch/epfl/tchu/game/arbitraryTest.java
@@ -1,71 +1,115 @@
 package ch.epfl.tchu.game;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
+import ch.epfl.tchu.SortedBag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class arbitraryTest {
+    final SortedBag<Card> testCards = SortedBag.of(Card.ALL.subList(0,4));
+    final Station[] stations = new Station[]{ new Station(0, "a"),
+            new Station(1, "b"),
+            new Station(2, "c"),
+            new Station(3, "d"),
+            new Station(4, "e")};
 
-    private Station s2;
-    private Station s1;
-    private Station s3;
-    private Station s4;
-    private Station s5;
-    private List<Station> allStations;
-
-    @BeforeEach
-    void setUp() {
-        this.s1 = new Station(0, "Station1");
-        this.s2 = new Station(1, "Station2");
-        this.s3 = new Station(2, "Station3");
-        this.s4 = new Station(3, "Station4");
-        this.s5 = new Station(3, "Station5");
-
-        this.allStations = List.of(s1, s2, s3, s4, s5);
+    @Test
+    void initial() {
+        assertThrows(IllegalArgumentException.class, () -> PlayerState.initial(SortedBag.of(Card.ALL)));
+        assertThrows(IllegalArgumentException.class, () -> PlayerState.initial(SortedBag.of()));
+        assertDoesNotThrow(() -> PlayerState.initial(SortedBag.of(Card.ALL.subList(0,4))));
     }
 
     @Test
-    void connectedWithStationsOfTheSameSubset() {
-        StationPartition partition = new StationPartition.Builder(2).connect(s1, s2).build();
-        assertTrue(partition.connected(s1, s2));
-        assertTrue(partition.connected(s1, s1));
+    void tickets() {
+        assertEquals(0, PlayerState.initial(testCards).tickets().size());
+        assertEquals(1, new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 2, Route.Level.OVERGROUND, null))).tickets().size());
+        assertEquals(4, new PlayerState(SortedBag.of(4, new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 2, Route.Level.OVERGROUND, null))).tickets().size());
+    }
 
-        StationPartition.Builder builder = new StationPartition.Builder(5);
-        builder.connect(s1, s2);
-        builder.connect(s1, s3);
-        assertTrue(builder.build().connected(s1, s2));
-        assertTrue(builder.build().connected(s2, s1));
-        assertTrue(builder.build().connected(s3, s1));
-        assertTrue(builder.build().connected(s3, s2));
+    @Test
+    void withAddedTickets() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 2, Route.Level.OVERGROUND, null)));
+        assertEquals(1, ps.tickets().size());
+        assertEquals(2, ps.withAddedTickets(SortedBag.of(new Ticket(stations[0], stations[1], 2))).tickets().size());
+        assertEquals(3, ps.withAddedTickets(SortedBag.of(2, new Ticket(stations[0], stations[1], 2))).tickets().size());
+    }
 
-        // Tries to add the same station to the builder
-        builder.connect(s3, s1);
-        assertTrue(builder.build().connected(s1, s3));
+    @Test
+    void withAddedCard() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 2, Route.Level.OVERGROUND, null)));
+        assertEquals(4, ps.cards().size());
+        assertEquals(5, ps.withAddedCard(Card.BLACK).cards().size());
 
-        // Add a new disjoint set of two station and connect them to the bug one.
-        builder.connect(s4, s5);
-        StationPartition partition2 = builder.build();
-        for (Station s : allStations) {
-            for (Station sPrime : allStations) {
-                assertTrue(partition2.connected(s, sPrime));
-            }
+    }
+
+    @Test
+    void withAddedCards() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 2, Route.Level.OVERGROUND, null)));
+        assertEquals(8, ps.withAddedCards(testCards).cards().size());
+    }
+
+    @Test
+    void canClaimRoute() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        PlayerState almostFullps = ps.withClaimedRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null), SortedBag.of());
+        assertTrue(almostFullps.withAddedCards(SortedBag.of(5, Card.RED)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, Color.RED)));
+        assertTrue(almostFullps.withAddedCards(SortedBag.of(5, Card.RED)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        assertFalse(almostFullps.withAddedCards(SortedBag.of(2, Card.RED)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        assertFalse(almostFullps.canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, Color.RED)));
+        for(int i = 0; i < 34; i+= 5) {
+            almostFullps = almostFullps.withClaimedRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null), SortedBag.of());
         }
+        PlayerState finalAlmostFullps = almostFullps;
+        assertFalse(finalAlmostFullps.withAddedCards(SortedBag.of(5, Card.RED)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        assertFalse(ps.withAddedCards(SortedBag.of(5, Card.LOCOMOTIVE)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, Color.RED)));
+        assertTrue(ps.withAddedCards(SortedBag.of(5, Card.LOCOMOTIVE)).canClaimRoute(new Route("r", stations[0], stations[1], 5, Route.Level.UNDERGROUND, Color.RED)));
     }
 
     @Test
-    void connectedWithDifferentSubset() {
-        StationPartition.Builder builder = new StationPartition.Builder(4);
-        builder.connect(s1, s2);
-        StationPartition partition = builder.build();
-        assertFalse(partition.connected(s1, s3));
+    void possibleClaimCards() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        assertEquals(1, ps.withAddedCards(SortedBag.of(5, Card.LOCOMOTIVE)).possibleClaimCards(new Route("r", stations[0], stations[1], 5, Route.Level.UNDERGROUND, Color.RED)).size());
+        assertEquals(6, ps.withAddedCards(SortedBag.of(5, Card.LOCOMOTIVE)).withAddedCards(SortedBag.of(5, Card.RED)).possibleClaimCards(new Route("r", stations[0], stations[1], 5, Route.Level.UNDERGROUND, Color.RED)).size());
     }
 
     @Test
-    void builderFails() {
-        assertThrows(IllegalArgumentException.class, () -> new StationPartition.Builder(-1));
-        assertDoesNotThrow(() -> new StationPartition.Builder(0));
+    void possibleAdditionalCards() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), SortedBag.of(), List.of(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        SortedBag looser = SortedBag.of(3, Card.GREEN);
+        PlayerState psWithAddedCards = ps.withAddedCards(SortedBag.of(2, Card.LOCOMOTIVE, 8, Card.GREEN));
+        PlayerState psWithLocos = ps.withAddedCards(SortedBag.of(8, Card.LOCOMOTIVE));
+        PlayerState anotherPs = ps.withAddedCards(SortedBag.of(8, Card.LOCOMOTIVE, 1, Card.GREEN));
+
+        assertEquals(1, psWithLocos.possibleAdditionalCards(3, SortedBag.of(5, Card.LOCOMOTIVE), looser).size()); //
+        assertEquals(0, psWithLocos.possibleAdditionalCards(3, SortedBag.of(7, Card.LOCOMOTIVE), looser).size()); // pas assez de locomotives, zut
+        assertEquals(0, psWithLocos.possibleAdditionalCards(2, SortedBag.of(7, Card.LOCOMOTIVE), looser).size()); // pas assez de locomotives, zut
+        assertEquals(1, psWithLocos.possibleAdditionalCards(1, SortedBag.of(7, Card.LOCOMOTIVE), looser).size()); // a 8-7 locomotives, il peut faire 1 choix
+        assertEquals(0, anotherPs.possibleAdditionalCards(1, SortedBag.of(8, Card.LOCOMOTIVE), looser).size()); // doit payer en locomotives
+
+        assertEquals(4, psWithAddedCards.withAddedCard(Card.LOCOMOTIVE).possibleAdditionalCards(3, SortedBag.of(4, Card.GREEN), looser).size());
+        assertEquals(3, psWithAddedCards.possibleAdditionalCards(3, SortedBag.of(4, Card.GREEN), looser).size());
+        assertEquals(3, psWithAddedCards.possibleAdditionalCards(2, SortedBag.of(4, Card.GREEN), looser).size());
+        assertEquals(1, psWithAddedCards.possibleAdditionalCards(2, SortedBag.of(8, Card.GREEN), looser).size());
+        assertEquals(0, psWithAddedCards.possibleAdditionalCards(2, SortedBag.of(8, Card.GREEN, 2, Card.LOCOMOTIVE), looser).size());
+    }
+
+    @Test
+    void withClaimedRoute() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, List.of(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null)));
+        assertEquals(0, ps.withClaimedRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null), testCards).cards().size());
+        assertEquals(2, ps.withClaimedRoute(new Route("r", stations[0], stations[1], 5, Route.Level.OVERGROUND, null), testCards).routes().size());
+    }
+
+    @Test
+    void ticketPoints() {
+        PlayerState ps = new PlayerState(SortedBag.of(new Ticket(stations[0], stations[1], 2)), testCards, Collections.emptyList());
+        assertEquals(-2, ps.ticketPoints());
+        assertEquals(-6, new PlayerState(SortedBag.of(3, new Ticket(stations[0], stations[1], 2)), testCards, Collections.emptyList()).ticketPoints());
+        assertEquals(6, new PlayerState(SortedBag.of(3, new Ticket(stations[0], stations[1], 2)), testCards, Collections.emptyList()).withClaimedRoute(new Route("a", stations[0], stations[1], 2, Route.Level.UNDERGROUND, null), SortedBag.of()).ticketPoints());
+        assertEquals(3, new PlayerState(SortedBag.of(3, new Ticket(stations[0], stations[1], 2), 1, new Ticket(stations[2], stations[3], 3)), testCards, Collections.emptyList()).withClaimedRoute(new Route("a", stations[0], stations[1], 2, Route.Level.UNDERGROUND, null), SortedBag.of()).ticketPoints());
     }
 }
