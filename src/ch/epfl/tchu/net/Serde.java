486192a11a20a07e38d3e1e4069db43fd12819a7
jerad <selim.jerad@epfl.ch>
2021-04-19 13:45:33 +0200

    etape7

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -2,6 +2,7 @@ package ch.epfl.tchu.net;
 
 import ch.epfl.tchu.SortedBag;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,8 +16,7 @@ public interface Serde <T>{
         return new Serde<T>() {
             @Override
             public String serialize(T t) {
-                return serialize.apply(t);
-            }
+                return serialize.apply(t); }
 
             @Override
             public T deserialize(String s) {
@@ -25,17 +25,39 @@ public interface Serde <T>{
         };
     }
 
-    static <T> Serde<T> OneOf(List<T> ValEnum){
+    static <T> Serde<T> oneOf(List<T> ValEnum){
+
+
 
     }
 
 
-    static <T> Serde<List<T>> ListOf(Serde serde, char separator){
+    static <T> Serde<List<T>> listOf(Serde serde, char separator){
+        return new Serde<List<T>>() {
+            @Override
+            public String serialize(List<T> list) {
+                String string = "";
+                int i=0;
+                for (T t : list){
+                    i++;
+                    string += serde.serialize(t);
+                    if (i != list.size()){
+                        string += separator;
+                    }
+                }
+                return string;
+            }
+
+            @Override
+            public List<T> deserialize(String string) {
+                return null;
+            }
+        }
 
     }
 
 
-    static <T extends Comparable<T>> Serde<SortedBag<T>> BagOf(Serde serde, char separator){
+    static <T extends Comparable<T>> Serde<SortedBag<T>> bagOf(Serde serde, char separator){
 
     }
 
