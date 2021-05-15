458cc976ef6a494d05a9e057d6953052eca2cac7
selimjerad <selim.jerad@epfl.ch>
2021-05-15 23:16:42 +0200

    sheeeeeesh

--- src/ch/epfl/tchu/gui/GraphicalPlayerAdapter.java
+++ src/ch/epfl/tchu/gui/GraphicalPlayerAdapter.java
@@ -5,20 +5,31 @@ import ch.epfl.tchu.game.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import static javafx.application.Platform.runLater;
 
 public class GraphicalPlayerAdapter implements Player {
 
     GraphicalPlayer graphicalPlayer;
+    BlockingQueue<SortedBag<Ticket>> ticketsQueue;
+    BlockingQueue<TurnKind> turnKindQueue;
+    BlockingQueue<Route> routesQueue;
+    BlockingQueue<SortedBag<Card>> cardsQueue;
+    BlockingQueue<Integer> cardPlacementsQueue;
 
     public GraphicalPlayerAdapter(){
-
+        ticketsQueue = new ArrayBlockingQueue<>(1);
+        turnKindQueue = new ArrayBlockingQueue<>(1);
+        routesQueue = new ArrayBlockingQueue<>(1);
+        cardsQueue = new ArrayBlockingQueue<>(1);
+        cardPlacementsQueue = new ArrayBlockingQueue<>(1);
     }
 
     @Override
     public void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames) {
-
+        runLater(() -> graphicalPlayer=new GraphicalPlayer(ownId, playerNames));
     }
 
     @Override
@@ -28,46 +39,102 @@ public class GraphicalPlayerAdapter implements Player {
 
     @Override
     public void updateState(PublicGameState newState, PlayerState ownState) {
-
+        runLater(()-> graphicalPlayer.setState(newState, ownState));
     }
 
     @Override
     public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
 
+        ActionHandlers.ChooseTicketsHandler chooseTicketsHandler = (ticketsToChoose) -> {
+            putInQueue(ticketsQueue, ticketsToChoose);
+        };
+        runLater(() -> graphicalPlayer.chooseTickets(chooseTicketsHandler,tickets));
     }
 
     @Override
     public SortedBag<Ticket> chooseInitialTickets() {
-        return null;
+        return ticketsQueue.peek();
     }
 
     @Override
     public TurnKind nextTurn() {
-        return null;
+
+        ActionHandlers.DrawTicketsHandler drawTicketsHandler = () -> {
+            putInQueue(turnKindQueue, TurnKind.DRAW_TICKETS);
+        };
+
+        ActionHandlers.ClaimRouteHandler claimRouteHandler = (route,cards) -> {
+            putInQueue(turnKindQueue, TurnKind.CLAIM_ROUTE);
+            putInQueue(routesQueue, route);
+            putInQueue(cardsQueue, cards);
+        };
+
+        ActionHandlers.DrawCardHandler drawCardHandler = (i) -> {
+            putInQueue(cardPlacementsQueue, i);
+        };
+
+        runLater(() -> graphicalPlayer.startTurn(drawTicketsHandler, drawCardHandler, claimRouteHandler));
+
+        return retrieveFromQueue(turnKindQueue);
     }
 
     @Override
     public int drawSlot() {
-        return 0;
+        if (cardPlacementsQueue.peek() != null){
+            return cardPlacementsQueue.peek();
+        }
+        else{
+            ActionHandlers.DrawCardHandler drawCardHandler = (i) -> {
+                putInQueue(cardPlacementsQueue, i);
+            };
+            runLater(() -> graphicalPlayer.drawCard(drawCardHandler));
+            return retrieveFromQueue(cardPlacementsQueue);
+        }
     }
 
     @Override
     public Route claimedRoute() {
-        return null;
+        return retrieveFromQueue(routesQueue);
     }
 
     @Override
     public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
-        return null;
+        setInitialTicketChoice(options);
+        return chooseInitialTickets();
     }
 
     @Override
     public SortedBag<Card> initialClaimCards() {
-        return null;
+        return retrieveFromQueue(cardsQueue);
     }
 
     @Override
     public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
-        return null;
+
+        ActionHandlers.ChooseCardsHandler chooseCardsHandler = (cards) ->{
+            try{cardsQueue.put(cards);}
+            catch(InterruptedException e){ throw new Error(); }
+        };
+
+        runLater(() -> graphicalPlayer.chooseAdditionalCards(chooseCardsHandler, options));
+        return cardsQueue.peek();
+    }
+
+    private <T> T retrieveFromQueue(BlockingQueue<T> blockingQueue){
+        try{
+            return blockingQueue.take();
+        }
+        catch (InterruptedException e){
+            throw new Error();
+        }
+    }
+
+    private <T> void putInQueue(BlockingQueue<T> blockingQueue, T element){
+        try{
+            blockingQueue.put(element);
+        }
+        catch (InterruptedException e){
+            throw new Error();
+        }
     }
 }
--- src/ch/epfl/tchu/gui/GuiConstants.java
+++ src/ch/epfl/tchu/gui/GuiConstants.java
@@ -1,6 +1,9 @@
 package ch.epfl.tchu.gui;
 
 public class GuiConstants {
+
+    //constants used for the graphical user interface
+
     /**
      * Maximum number of messages displayed on the screen
      */
