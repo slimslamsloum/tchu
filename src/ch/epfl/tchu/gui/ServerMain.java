2a5a6829f0ed9ad44dd58d21337ce7558190f027
selimjerad <selim.jerad@epfl.ch>
2021-05-19 18:35:58 +0200

    j'ai fini tChu niquez vos meres

--- src/ch/epfl/tchu/gui/ServerMain.java
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -19,28 +19,22 @@ public class ServerMain extends Application {
 
     public static void main(String[] args) { launch(args); }
 
-
-    @Override
     public void start(Stage primaryStage) throws Exception {
-        List<String> params= getParameters().getRaw();
-        try (ServerSocket serverSocket = new ServerSocket(5108);
-             Socket socket = serverSocket.accept()) {
-            Player player1 = new GraphicalPlayerAdapter();
-            Player player2 = new RemotePlayerProxy(serverSocket.accept());
-            SortedBag<Ticket> tickets = SortedBag.of(ChMap.tickets());
-            Map<PlayerId, String> names =
-                    Map.of(PLAYER_1, "Ada", PLAYER_2, "Charles");
-            Map<PlayerId, Player> players =
-                    Map.of(PLAYER_1, player1,
-                            PLAYER_2, player2);
-            Random rng = new Random();
-            if (params.size()==2){
-                new Thread(() -> Game.play(players,names,tickets,rng)).start();
-            }
-            else {
-                new Thread( () ->  Game.play(players,
-                        Map.of(PLAYER_1, params.get(0), PLAYER_2, params.get(1)),tickets, rng)).start();
-            }
-        }
+        String name1 = getParameters().getRaw().isEmpty() ?
+                "Ada" : getParameters().getRaw().get(0);
+        String name2 = getParameters().getRaw().isEmpty() ?
+                "Charles" : getParameters().getRaw().get(1);
+        ServerSocket serverSocket = new ServerSocket(5108);
+        Socket socket = serverSocket.accept();
+        Player player1 = new GraphicalPlayerAdapter();
+        Player player2 = new RemotePlayerProxy(socket);
+        SortedBag<Ticket> tickets = SortedBag.of(ChMap.tickets());
+        Map<PlayerId, String> names =
+                Map.of(PLAYER_1, name1, PLAYER_2, name2);
+        Map<PlayerId, Player> players =
+                Map.of(PLAYER_1, player1,
+                        PLAYER_2, player2);
+        Random rng = new Random();
+        new Thread(() -> Game.play(players,names,tickets,rng)).start();
     }
 }
